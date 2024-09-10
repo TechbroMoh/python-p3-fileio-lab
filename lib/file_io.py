57db064d26a2b77b550433810d488da743dd15ee
felix <felixmonari400@gmail.com>
2024-09-10 05:58:13 +0300

    Use pathlib.Path for file operations

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,16 @@
+from pathlib import Path
+
 def write_file(file_name, file_content):
-    pass
+    file_path = Path(file_name).with_suffix('.txt')
+    with file_path.open('w') as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    file_path = Path(file_name).with_suffix('.txt')
+    with file_path.open('a') as file:
+        file.write(append_content)
 
 def read_file(file_name):
-    pass
+    file_path = Path(file_name).with_suffix('.txt')
+    with file_path.open('r') as file:
+        return file.read()
